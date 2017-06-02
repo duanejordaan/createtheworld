e1c3e6cff1a9ca702c393a2b6de1435ae2ce70cd
Duane Jordaan <duanejordaan@Duanes-Mac.local>
2017-06-02 21:54:07 +0200

    Initial Commit

---
+++ CreateTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CreateTheWorld
+//
+//  Created by Duane Jordaan on 2017/06/02.
+//  Copyright © 2017 Duane Jordaan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
